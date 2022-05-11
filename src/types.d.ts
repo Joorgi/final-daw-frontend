5ade41c03a79e67d40cb98a5789d56848e3a1eb1
joorgi <joorgi@floridauniversitaria.es>
2022-05-11 04:38:51 +0200

    Add models types

---
+++ src/types.d.ts
@@ -0,0 +1,15 @@
+export interface Marca {
+	makeName: string
+	makeDate: Date
+	makeImg: string
+}
+
+export interface Modelo {
+	modelName: string
+	makeId: number
+	makeImg: number[]
+}
+
+export interface Imagen {
+	img: string
+}
