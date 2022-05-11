3c26db5b6fe92e9e96deff824792b42a782f65d8
joorgi <joorgi@floridauniversitaria.es>
2022-05-11 04:41:31 +0200

    Add marcaService

---
+++ src/services/marcaService.ts
@@ -0,0 +1,48 @@
+import { API_URL } from './settings'
+import axios from 'axios'
+import { Marca } from '../types'
+
+export const getMarcas = async () => {
+	try {
+		const request = axios.get(API_URL + '/marcas')
+		return request.then(response => response.data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const getMarcaById = async (id: number) => {
+	try {
+		const request = axios.get(API_URL + `/marca/${id}`)
+		return request.then(response => response.data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const createMarca = async (marca: Marca) => {
+	try {
+		const { data } = await axios.post(API_URL + '/marca/new', marca)
+		return data
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const updateMarca = async (id: number, marca: Marca) => {
+	try {
+		const { data } = await axios.post(API_URL + `/marca/${id}`, marca)
+		return data
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const deleteMarca = async (id: number) => {
+	try {
+		await axios.post(API_URL + `/marca/${id}`)
+		return true
+	} catch (error) {
+		console.log(error)
+	}
+}
