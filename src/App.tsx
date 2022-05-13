b6a2243b9eaaf4b848ec63d8e1b6b9724bb048c2
joorgi <joorgi@floridauniversitaria.es>
2022-05-13 18:33:37 +0200

    Add flowbite package and Montserrat font family

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import { Switch, Route } from 'wouter'
+import LandigPage from './pages/landigPage'
+import Navbar from './components/NavBar'
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type='button' onClick={() => setCount(count => count + 1)}>
-						count is: {count}
-					</button>
-				</p>
-				<p>
-					Edit <code>App.tsx</code> and save to test HMR updates.
-				</p>
-				<p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn React
-					</a>
-					{' | '}
-					<a
-						className='App-link'
-						href='https://vitejs.dev/guide/features.html'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
+			<Navbar />
+			<main>
+				<Switch>
+					<Route component={LandigPage} path='/'></Route>
+					<Route component={LandigPage} path='/:anything*'></Route>
+				</Switch>
+			</main>
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 }
