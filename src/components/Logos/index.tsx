4591da7a7a301493a7f041c375fe91237e4bffe0
joorgi <joorgi@floridauniversitaria.es>
2022-05-19 02:55:05 +0200

    BrandsLogos added

---
+++ src/components/Logos/index.tsx
@@ -0,0 +1,36 @@
+export default function BrandsLogos() {
+	return (
+		<section id='logos__section'>
+			<div className='flex justify-center gap-16 py-8'>
+				<div id='logos__content'>
+					<img
+						src='https://raw.githubusercontent.com/bedimcode/responsive-car-website/main/assets/img/logo1.png'
+						alt='logos__img'
+						className='w-[4rem] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 bg-indigo-500'
+					/>
+				</div>
+				<div>
+					<img
+						src='https://raw.githubusercontent.com/bedimcode/responsive-car-website/main/assets/img/logo1.png'
+						alt=''
+						className='w-[4rem] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 bg-indigo-500'
+					/>
+				</div>
+				<div>
+					<img
+						src='https://raw.githubusercontent.com/bedimcode/responsive-car-website/main/assets/img/logo1.png'
+						alt=''
+						className='w-[4rem] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 bg-indigo-500'
+					/>
+				</div>
+				<div>
+					<img
+						src='https://raw.githubusercontent.com/bedimcode/responsive-car-website/main/assets/img/logo1.png'
+						alt=''
+						className='w-[4rem] p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 bg-indigo-500'
+					/>
+				</div>
+			</div>
+		</section>
+	)
+}
