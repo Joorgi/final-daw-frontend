2019c9fb0744044f9308f8e2fef588762a99e740
joorgi <joorgi@floridauniversitaria.es>
2022-05-19 03:03:07 +0200

    Animations added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				shake: {
+					'0%': { transform: 'translate(0)' },
+					'20%': { transform: 'translate(-2px, 2px)' },
+					'40%': { transform: 'translate(-2px, -2px)' },
+					'60%': { transform: 'translate(2px, 2px)' },
+					'80%': { transform: 'translate(2px, -2px)' },
+					'100%': { transform: 'translate(0)' },
+				},
+			},
+			animation: {
+				shake: 'shake 0.3s linear both',
+			},
+		},
 	},
 	plugins: [require('flowbite/plugin')],
 }
